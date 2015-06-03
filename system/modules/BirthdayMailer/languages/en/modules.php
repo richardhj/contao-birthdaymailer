fb0845106bc5a82f397cd68c9efd8b111b18a887
Cliff Parnitzky <github@cliff-parnitzky.de>
2015-06-03 11:24:31 +0200

    Corrected typo

--- system/modules/BirthdayMailer/languages/en/modules.php
+++ system/modules/BirthdayMailer/languages/en/modules.php
@@ -30,6 +30,6 @@
 /**
  * Define name and tooltip for preferences (inactive modules)
  */
-$GLOBALS['TL_LANG']['MOD']['BirthdayMailer'] = array('Birthdaymailer', 'Sends a birthday email to all the members having their birtday on the current day.');
+$GLOBALS['TL_LANG']['MOD']['BirthdayMailer'] = array('Birthday mailer', 'Sends a birthday email to all the members having their birtday on the current day.');
 
 ?>
